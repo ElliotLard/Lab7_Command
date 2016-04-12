6fd338eb47201535119e2077a428365648166090
Chris <Chris@Chris-PC>
2016-04-12 02:03:23 -0400

    Updated:
    GUI test is complete and GUI now displays correct symbols for Aliens,
    humans, and weapons.

--- lab7_Command/src/gui/GUI.java
+++ lab7_Command/src/gui/GUI.java
@@ -23,13 +23,11 @@ import lifeform.Alien;
 import lifeform.Human;
 
 public class GUI extends JFrame implements ActionListener
-{
-	/**
-	 * Just leave these be for now. Won't need them by turn in but it's
-	 * going to be mad if they're removed without also removing them elsewhere
-	 */
-	JButton textButton, imageButton;
-	JLabel textLabel, imageLabel;	
+{	
+	int what[] = {0, 10, 20};
+	int huh[] = {20, 0, 20};
+	int derp = 3;
+	
 	
 	public GUI(){
 		
@@ -47,22 +45,38 @@ public class GUI extends JFrame implements ActionListener
 		setLayout (new BorderLayout());
 		
 		/**
-		 * Just a spot filler for now. Will be replaced with a map legend.
+		 * Makes the map key for the left panel of the frame.
+		 * Going to keep it this way for now because it works,
+		 * but probably should see if there's a way to shrink it.
 		 */
-		JPanel mapKey = new JPanel (new GridLayout(6, 1));
-		JLabel[][] keyArray = new JLabel[6][1];
-		keyArray[0][0] = new JLabel ("A - Alien");
+		JPanel mapKey = new JPanel (new GridLayout(5, 1));
+		JLabel[][] keyArray = new JLabel[5][1];
+		
+		keyArray[0][0] = new JLabel (createTriangle());
 		mapKey.add(keyArray[0][0]);
-		keyArray[1][0] = new JLabel ("H - Human");
+		keyArray[0][0] = new JLabel (" - Alien");
+		mapKey.add(keyArray[0][0]);
+		
+		keyArray[1][0] = new JLabel (createCircle());
 		mapKey.add(keyArray[1][0]);
-		keyArray[2][0] = new JLabel ("P - Pistol");
+		keyArray[1][0] = new JLabel (" - Human");
+		mapKey.add(keyArray[1][0]);
+		
+		keyArray[2][0] = new JLabel (createPistol());
 		mapKey.add(keyArray[2][0]);
-		keyArray[3][0] = new JLabel ("C - ChainGun");
+		keyArray[2][0] = new JLabel (" - Pistol");
+		mapKey.add(keyArray[2][0]);
+		
+		keyArray[3][0] = new JLabel (createChainGun());
 		mapKey.add(keyArray[3][0]);
-		keyArray[4][0] = new JLabel ("L - PlasmaCannon");
+		keyArray[3][0] = new JLabel (" - ChainGun");
+		mapKey.add(keyArray[3][0]);
+		
+		keyArray[4][0] = new JLabel (createPlasmaCannon());
 		mapKey.add(keyArray[4][0]);
-		keyArray[5][0] = new JLabel (". - Empty");
-		mapKey.add(keyArray[5][0]);
+		keyArray[4][0] = new JLabel (" - PlasmaCannon ");
+		mapKey.add(keyArray[4][0]);
+		
 		add("East", mapKey);
 		
 		/**
@@ -87,14 +101,14 @@ public class GUI extends JFrame implements ActionListener
 				 */
 				if (Environment.getWorld().getLifeForm(x, y) instanceof Alien)
 				{
-					labelArray[x][y] = new JLabel ("  A  ");
+					labelArray[x][y] = new JLabel (createTriangle());
 				}
 				/**
 				 * Checks for a Human and places an H if it finds one.
 				 */
 				else if (Environment.getWorld().getLifeForm(x, y) instanceof Human)
 				{
-					labelArray[x][y] = new JLabel ("  H  ");
+					labelArray[x][y] = new JLabel (createCircle());
 				}
 				/**
 				 * If nothing is found it places a dot. Spacing is to keep
@@ -103,19 +117,19 @@ public class GUI extends JFrame implements ActionListener
 				
 				else if (Environment.getWorld().getWeapon(x, y) instanceof Pistol)
 				{
-					labelArray[x][y] = new JLabel ("  P  ");
+					labelArray[x][y] = new JLabel (createPistol());
 				}
 				else if (Environment.getWorld().getWeapon(x, y) instanceof ChainGun)
 				{
-					labelArray[x][y] = new JLabel ("  C  ");
+					labelArray[x][y] = new JLabel (createChainGun());
 				}
 				else if (Environment.getWorld().getWeapon(x, y) instanceof PlasmaCannon)
 				{
-					labelArray[x][y] = new JLabel ("  L  ");
+					labelArray[x][y] = new JLabel (createPlasmaCannon());
 				}
 				else
 				{
-					labelArray[x][y] = new JLabel ("  .  ");
+					labelArray[x][y] = new JLabel (createGround());
 				}
 				/**
 				 * Sets centerPanel to hold the labelArray after it is created.
@@ -134,6 +148,75 @@ public class GUI extends JFrame implements ActionListener
 		 */
 		pack();
 		setVisible(true);
+	}
+	
+	public ImageIcon createTriangle(){
+		BufferedImage Image = new
+				BufferedImage (20, 20, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics drawer = Image.getGraphics();
+		
+		drawer.setColor(new Color(0, 255, 0));
+		drawer.fillPolygon(what, huh,  derp);
+		
+		return new ImageIcon(Image);
+	}
+	
+	public ImageIcon createCircle(){
+		BufferedImage Image = new
+				BufferedImage (20, 20, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics drawer = Image.getGraphics();
+		
+		drawer.setColor(new Color(255, 0, 0));
+		drawer.fillOval(0,  0,  20,  20);
+		
+		return new ImageIcon(Image);
+	}
+	
+	public ImageIcon createGround(){
+		BufferedImage Image = new
+				BufferedImage (20, 20, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics drawer = Image.getGraphics();
+		
+		drawer.setColor(new Color(200, 200, 200));
+		drawer.fillRect(0,  0,  20,  20);
+		
+		drawer.setColor(new Color(0, 0, 0));
+		drawer.drawRect(0, 0, 20, 20);
+		
+		return new ImageIcon(Image);
+	}
+	
+	public ImageIcon createPistol(){
+		BufferedImage Image = new
+				BufferedImage (20, 20, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics drawer = Image.getGraphics();
+		
+		drawer.setColor(new Color(255, 255, 0));
+		drawer.fillOval(5,  5,  10,  10);
+		
+		return new ImageIcon(Image);
+	}
+	
+	public ImageIcon createChainGun(){
+		BufferedImage Image = new
+				BufferedImage (20, 20, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics drawer = Image.getGraphics();
+		
+		drawer.setColor(new Color(0, 255, 255));
+		drawer.fillOval(5,  5,  10,  10);
+		
+		return new ImageIcon(Image);
+	}
+	
+	public ImageIcon createPlasmaCannon(){
+		BufferedImage Image = new
+				BufferedImage (20, 20, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics drawer = Image.getGraphics();
+		
+		drawer.setColor(new Color(255, 0, 255));
+		drawer.fillOval(5,  5,  10,  10);
+		
+		return new ImageIcon(Image);
 	}
 
 	/**
--- lab7_Command/tests/gui/TestGUI.java
+++ lab7_Command/tests/gui/TestGUI.java
@@ -2,6 +2,8 @@ package gui;
 
 import static org.junit.Assert.*;
 
+import javax.swing.JOptionPane;
+
 import org.junit.Test;
 
 public class TestGUI
@@ -9,6 +11,10 @@ public class TestGUI
 	@Test
 	public void testInitialization()
 	{
+		assertEquals(JOptionPane.YES_OPTION, JOptionPane.showConfirmDialog
+				(null,  "Create Cell Image Icon Correct For\nHuman(0,0) and Alien(1,1)\nDoes it look right?"));
 		
+		assertEquals(JOptionPane.YES_OPTION, JOptionPane.showConfirmDialog
+				(null,  "Map Legend Displays Correctly\nAlien(Green Triangle)\nDoes it look right?"));
 	}
 }
